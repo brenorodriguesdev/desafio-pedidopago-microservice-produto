394398d7c0e228f966223ac94546fa6c3ad1db3e
“Breno <brenorodriguesdev@gmail.com>
2021-12-01 19:51:23 -0300

    feat: add criar produtos controller

---
+++ src/presentation/controllers/criar-produtos.ts
@@ -0,0 +1,32 @@
+import { CriarProdutosUseCase } from "../../domain/useCases/criar-produtos";
+import { Validator } from "../../validation/contracts/validator";
+import { Controller } from "../contracts/controller";
+import { GRPCRequest } from "../contracts/grpc";
+import { MissingParamError } from "../errors/missing-param-error";
+
+export class CriarProdutosController implements Controller {
+    constructor(private readonly validator: Validator, private readonly criarProdutosUseCase: CriarProdutosUseCase) { }
+    async handle(grpcRequest: GRPCRequest): Promise<any> {
+
+        if (!grpcRequest.request.produtos) {
+            throw new MissingParamError('produtos')
+        }
+
+        for (let produto of grpcRequest.request.produtos) {
+            const error = this.validator.validate(produto)
+
+            if (error) {
+                throw error
+            }
+        }
+        
+
+        const {
+            produtos
+        } = grpcRequest.request
+
+        const produto = await this.criarProdutosUseCase.criar(produtos)
+        
+        return produto
+    }
+}
