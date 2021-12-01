930036ce2a3eec7640b1659b3e69b3c1e370d306
“Breno <brenorodriguesdev@gmail.com>
2021-12-01 20:23:11 -0300

    feat: add clonar produto controller factory

---
+++ src/main/factories/controllers/clonar-produto.ts
@@ -0,0 +1,14 @@
+import { ClonarProdutoService } from "../../../data/services/clonar-produto"
+import { ProdutoIngredienteRepositoryORM } from "../../../infra/produto-ingrediente-repository"
+import { ProdutoRepositoryTypeORM } from "../../../infra/produto-repository"
+import { Controller } from "../../../presentation/contracts/controller"
+import { ClonarProdutoController } from "../../../presentation/controllers/clonar-produto"
+import { makeClonarProdutoValidator } from "../validators/clonar-produto"
+
+export const makClonaProdutoController = (): Controller => {
+    const produtoRepository = new ProdutoRepositoryTypeORM()
+    const produtoIngredienteRepository = new ProdutoIngredienteRepositoryORM()
+
+    const clonarProdutoService = new ClonarProdutoService(produtoRepository, produtoIngredienteRepository)
+    return new ClonarProdutoController(makeClonarProdutoValidator(), clonarProdutoService)
+}
